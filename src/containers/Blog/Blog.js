07c78c43a0d8fcf8abc6cf2cdd5d1bc77c223061
smithwebtek <brad@smithwebtek.com>
2017-12-22 15:52:55 -0500

    update folder structure for routing

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,57 +1,21 @@
 import React, { Component } from 'react';
-// import axios from 'axios';
-import axios from '../../axios';
-
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
+// import axios from '../../axios';
 import './Blog.css';
- 
+import Posts from './Posts/Posts';
+
 class Blog extends Component {
-  state = {
-    posts: [],
-    selectedPostId: null, 
-    error: false
-  }
-
-  componentDidMount(){
-    axios.get('/posts')
-      .then(response => {
-        const posts = response.data.slice(0, 4)
-        const updatedPosts = posts.map(post => {
-          return {
-            ...post, 
-            author: 'Brad'
-          }
-        });
-        this.setState({posts: updatedPosts});
-      })
-      .catch(error => {
-        this.setState({error: true});
-      });
-    }
-
-  postSelectedHandler = (id) => {
-    this.setState({selectedPostId: id});
-  } 
-
   render () {
-    let posts = <p style={{textAlign: 'center'}}>Something went wrong!</p>
-    if(!this.state.error) {
-      posts = this.state.posts.map(post => {
-        return <Post
-          key={post.id} 
-          title={post.title}
-          author={post.author}
-          clicked={() => this.postSelectedHandler(post.id)}/>
-      });
-    }
-
     return (
       <div>
-        <section className="Posts">
-          {posts}
-        </section>
+        <header>
+          <nav>
+            <ul>
+              <li><a href="/" alt="home">Home</a></li>
+              <li><a href="/" alt="new-post">New Post</a></li>
+            </ul>
+          </nav>
+        </header>
+        <Posts />
       </div>
       );
     }
